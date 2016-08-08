9bcb42def10add9494ce8e3d2010e2a0cd8b5f81
Yatin <yatin.gera5@gmail.com>
2016-08-08 09:47:48 -0700

    Update auth.js

--- config/auth.js
+++ config/auth.js
@@ -4,8 +4,8 @@
 var auth = {};
 
 auth.google = {
-    clientID: '959400446018-9r3bota6jtlegc2g124oju4025spiahq.apps.googleusercontent.com',
-    clientSecret: 'bE2OhCT9gIwrRBNjjn4bW9Bs',
+    clientID: 'xxxxxx',
+    clientSecret: 'xxxxxx',
     callbackURL : 'http://localhost:3000/accounts/auth/google/callback'
 
 };
